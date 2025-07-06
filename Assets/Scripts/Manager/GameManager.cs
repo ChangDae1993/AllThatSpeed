3b357f38854661d8684cbe0c26dcf1dd254d285f
ChangDae1993 <jsj939639@gmail.com>
2025-07-06 12:35:44 +0900

    3nd Test commit

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager GM;
+
+    private void Awake()
+    {
+        if (GM == null)
+        {
+            DontDestroyOnLoad(gameObject);
+
+            GM = this;
+        }
+        else if (GM != this)
+        {
+
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
